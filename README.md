8bbfa190a303ee62df31e1eb431aaecf3cc6fbbc
mhmtkarakas <m.karakas32@gmail.com>
2023-03-21 23:08:02 +0300

    added basket and screen gif

---
+++ README.md
@@ -0,0 +1,25 @@
+# E-Commerce Website
+
+<p>This project is a web-based e-commerce platform built using HTML, CSS, and JavaScript. It allows users to browse products, add them to their cart, and checkout using a simple and intuitive interface.</p>
+
+## Features
+
+<ul>
+ <li>Browse products by category or search by keyword</li>
+ <li>Add products to cart and update quantity</li>
+ <li>Remove products from cart</li>
+ <li>Checkout using a secure payment gateway</li>
+ <li>View order history</li>
+</ul>
+
+## Installation
+
+1. Clone the repository from GitHub:
+ 
+ git clone https://github.com/mhmtkarakas/E-COMMERCE--with-JAVASCRIPT-API.git
+
+2. Open the project folder and launch index.html in your browser.
+
+## Screen Gif
+
+![](ekran.gif)
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const categoryList = document.querySelector(".category-list");
 const productList = document.querySelector(".product-list");
-const sepetBtn = document.querySelector('#sepet');
-const modal = document.querySelector('.modal-wrapper')
-const closeBtn = document.querySelector('#close');
-
+const sepetBtn = document.querySelector("#sepet");
+const modal = document.querySelector(".modal-wrapper");
+const closeBtn = document.querySelector("#close");
+const modalList = document.getElementById("modal-list");
+const fiyatSpan = document.querySelector('#fiyat')
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchCategories();
@@ -39,18 +40,60 @@ function fetchProducts() {
             <p>${product.category.name}</p>
             <div class="product-info">
                 <span>${product.price}</span>
-            <button>Sepete Ekle</button>
-        `
+                <button onclick="sepeteEkle({name:'${product.title}',id:'${product.id}',price:'${product.price}',amount:1})" >Sepete Ekle</button>
+        `;
         productList.appendChild(productDiv);
       })
     );
 }
 
 // sepeti acma ve kapama
-sepetBtn.addEventListener('click',toggleSepet);
-closeBtn.addEventListener('click',toggleSepet);
-
-function toggleSepet(){
-   modal.classList.toggle('active')
+const basket=[];
+let toplamFiyat = 0
+function listBasket() {
+     basket.forEach((item)=>{
+      
+      // SEPET ELEMANININ DIVINI OLUSTURMA
+      const basketItem = document.createElement('div');
+      basketItem.classList.add('sepetItem');
+      basketItem.innerHTML = `
+            <h2>${item.name}</h2>
+            <h2>${item.price} TL</h2>
+            <p>${item.amount}</p>
+      `
+      modalList.appendChild(basketItem);
+      toplamFiyat += Number(item.price)*item.amount
+      
+     })
+     fiyatSpan.innerText = toplamFiyat
 }
+
+sepetBtn.addEventListener("click", ()=>{
+  // SEPETI ACAR
+  toggleSepet();
+  //sepete elemanlari ekler
+  listBasket()
+});
+closeBtn.addEventListener("click", ()=>{
+  //sepeti kapatir
+  toggleSepet();
+  //sepete elemanlari ekler
+  modalList.innerHTML='';
+});
+
+function toggleSepet() {
+  modal.classList.toggle("active");
+}
+
+// SEPETE ELEMAN EKLEME
+
+function sepeteEkle(param) {
+  const foundItem = basket.find((item)=>item.id == param.id);
+  if (foundItem) {
+    foundItem.amount++;
+  } else {
+    basket.push(param);
+  }
+}
+
 
